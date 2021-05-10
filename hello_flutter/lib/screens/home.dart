acc8ac2c61aebbc424228ff9e0cc0665a6cf5a31
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2021-05-10 21:59:14 +0200

    Adding Your Logic

--- hello_flutter/lib/screens/home.dart
+++ hello_flutter/lib/screens/home.dart
@@ -7,11 +7,30 @@ class Home extends StatelessWidget {
       color:  Colors.deepPurple,
       child: Center(
         child: Text(
-          "Hello Flutter!",
+          sayHello(),
           textDirection: TextDirection.ltr,
           style: TextStyle(color: Colors.white, fontSize: 36.0),
         )
       ),
     );
+  }
+
+  String sayHello() {
+    var now = DateTime.now();
+    String hello;
+
+    if (now.hour < 12) {
+      hello = "Good Morning!";
+    } else if (now.hour < 18) {
+      hello = "Good Afternoon!";
+    } else {
+      hello = "Good Evening!";
+    }
+
+    var minutes = now.minute < 10
+      ? "0" + now.minute.toString()
+      : now.minute.toString();
+
+    return "It's now " + now.hour.toString() + ":" + minutes + ".\n" + hello;
   }
 }
